1cc8bd3c4e99640d95ca138021e496bd767e0ed4
Alegria Aclan <aaclan@ebi.ac.uk>
2021-07-08 11:41:55 +0100

    Improve setup code on some tests

--- tests/unit/importer/test_submission.py
+++ tests/unit/importer/test_submission.py
@@ -184,12 +184,16 @@ def _create_spreadsheet_json():
 
 class IngestSubmitterTest(TestCase):
 
+    def setUp(self) -> None:
+        self.ingest_api = MagicMock('mock_ingest_api')
+        self.ingest_api.get_submission = MagicMock()
+        self.ingest_api.create_submission_manifest = MagicMock()
+        self.ingest_api.patch = MagicMock()
+        self.ingest_api.get_link_from_resource = MagicMock()
+
     @patch('ingest.importer.submission.ingest_submitter.Submission')
     def test_submit(self, submission_constructor):
         # given:
-        ingest_api = MagicMock('mock_ingest_api')
-        ingest_api.get_submission = MagicMock()
-        ingest_api.create_submission_manifest = MagicMock()
         submission = self._mock_submission(submission_constructor)
 
         # and:
@@ -199,28 +203,22 @@ class IngestSubmitterTest(TestCase):
         entity_map = EntityMap(product, user, project)
 
         # when:
-        submitter = IngestSubmitter(ingest_api)
+        submitter = IngestSubmitter(self.ingest_api)
         submitter.add_entities(entity_map, submission_url='url')
 
         # then:
-        submission_constructor.assert_called_with(ingest_api, 'url')
+        submission_constructor.assert_called_with(self.ingest_api, 'url')
         submission.define_manifest.assert_called_with(entity_map)
         submission.add_entity.assert_has_calls([call(product), call(user)], any_order=True)
 
     def test_update_entities(self):
         # given:
-        ingest_api = MagicMock('mock_ingest_api')
-        ingest_api.get_submission = MagicMock()
-        ingest_api.create_submission_manifest = MagicMock()
-        ingest_api.patch = MagicMock()
-
-        # and:
         entity_map = self._create_test_entity_map()
         user2 = entity_map.get_entity('user', 'user_2')
         user3 = entity_map.get_entity('user', 'user_3')
 
         # when:
-        submitter = IngestSubmitter(ingest_api)
+        submitter = IngestSubmitter(self.ingest_api)
         submitter.update_entity = MagicMock()
         submitter.update_entities(entity_map)
 
@@ -241,11 +239,6 @@ class IngestSubmitterTest(TestCase):
     @patch('ingest.importer.submission.ingest_submitter.Submission')
     def test_submit_linked_entity(self, submission_constructor):
         # given:
-        ingest_api = MagicMock('mock_ingest_api')
-        ingest_api.get_submission = MagicMock()
-        ingest_api.create_submission_manifest = MagicMock()
-        ingest_api.patch = MagicMock()
-        ingest_api.get_link_from_resource = MagicMock()
         submission = self._mock_submission(submission_constructor)
 
         # and:
@@ -265,66 +258,58 @@ class IngestSubmitterTest(TestCase):
         entity_map.add_entity(project)
 
         # when:
-        submitter = IngestSubmitter(ingest_api)
+        submitter = IngestSubmitter(self.ingest_api)
         submitter.link_submission_to_project = MagicMock()
         submitter.PROGRESS_CTR = 1
         submitter.add_entities(entity_map, submission_url='url')
 
         # then:
-        submission_constructor.assert_called_with(ingest_api, 'url')
+        submission_constructor.assert_called_with(self.ingest_api, 'url')
         submission.define_manifest.assert_called_with(entity_map)
         submission.add_entity.assert_has_calls([call(user), call(linked_product)], any_order=True)
         submission.link_entity.assert_called_with(linked_product, user, relationship='wish_list', is_collection=True)
-        ingest_api.patch.assert_called_once()
+        self.ingest_api.patch.assert_called_once()
 
     def test_update_entity(self):
         # given:
-        ingest_api = MagicMock('mock_ingest_api')
-        ingest_api.patch = MagicMock()
-
-        # and:
         user1 = Entity('user', 'user_1', {'k': 'v2'}, {'content': {'k': 'v', 'k2': 'v2'}, '_links': {'self': {'href': 'url'}}})
 
         # when:
-        submitter = IngestSubmitter(ingest_api)
+        submitter = IngestSubmitter(self.ingest_api)
         submitter.update_entity(user1)
 
         # then:
-        ingest_api.patch.assert_called_with('url', {'content': {'k': 'v2', 'k2': 'v2'}})
+        self.ingest_api.patch.assert_called_with('url', {'content': {'k': 'v2', 'k2': 'v2'}})
 
     def test_update_entity__given_empty_ingest_json__then_fetch_resource(self):
         # given:
-        ingest_api = MagicMock('mock_ingest_api')
-        ingest_api.patch = MagicMock()
         ingest_json = {'content': {'k': 'v', 'k2': 'v2'}, '_links': {'self': {'href': 'url'}}}
-        ingest_api.get_entity_by_uuid = Mock(return_value=ingest_json)
+        self.ingest_api.get_entity_by_uuid = Mock(return_value=ingest_json)
 
         # and:
         user1 = Entity('biomaterial', 'biomaterial_uuid', {'k': 'v2'}, None)
 
         # when:
-        submitter = IngestSubmitter(ingest_api)
+        submitter = IngestSubmitter(self.ingest_api)
         submitter.update_entity(user1)
 
         # then:
-        ingest_api.patch.assert_called_with('url', {'content': {'k': 'v2', 'k2': 'v2'}})
+        self.ingest_api.patch.assert_called_with('url', {'content': {'k': 'v2', 'k2': 'v2'}})
 
     def test_update_entity__given_content_has_no_update__then_do_not_patch(self):
         # given:
-        ingest_api = MagicMock('mock_ingest_api')
-        ingest_api.patch = MagicMock()
         ingest_json = {'content': {'k': 'v2', 'k2': 'v2'}, '_links': {'self': {'href': 'url'}}}
-        ingest_api.get_entity_by_uuid = Mock(return_value=ingest_json)
+        self.ingest_api.get_entity_by_uuid = Mock(return_value=ingest_json)
 
         # and:
         user1 = Entity('biomaterial', 'biomaterial_uuid', {'k': 'v2'}, None)
 
         # when:
-        submitter = IngestSubmitter(ingest_api)
+        submitter = IngestSubmitter(self.ingest_api)
         submitter.update_entity(user1)
 
         # then:
-        ingest_api.patch.assert_not_called()
+        self.ingest_api.patch.assert_not_called()
 
     @staticmethod
     def _mock_submission(submission_constructor):
