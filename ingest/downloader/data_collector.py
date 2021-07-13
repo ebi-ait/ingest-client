9259ef08e101c31ac53ebca783ada7ad028d6ec9
Karoly Erdos <karoly@ebi.ac.uk>
2021-07-13 12:28:54 +0100

    Add biomaterials to the collected data

--- ingest/downloader/data_collector.py
+++ ingest/downloader/data_collector.py
@@ -9,9 +9,17 @@ class DataCollector:
     def collect_data_by_submission_uuid(self, submission_uuid):
         submission = self.api.get_submission_by_uuid(submission_uuid).json
 
-        project_json = self.api.get_related_entities('relatedProjects', submission, 'project').json
-
+        project_json = self.api.get_related_entities('relatedProjects', submission, 'projects').json
         data_by_submission = [
             project_json
         ]
+
+        self.__get_biomaterials(data_by_submission, submission)
+
         return data_by_submission
+
+    def __get_biomaterials(self, data_by_submission, submission):
+        biomaterials_json = self.api.get_related_entities('biomaterials', submission, 'biomaterials').json
+        if biomaterials_json:
+            biomaterials_json = biomaterials_json['_embedded']['biomaterials']
+            data_by_submission.extend(biomaterials_json)
--- tests/unit/downloader/test_data_collector.py
+++ tests/unit/downloader/test_data_collector.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock
 
 from ingest.api.ingestapi import IngestApi
 from ingest.downloader.data_collector import DataCollector
@@ -13,18 +13,25 @@ class MyTestCase(unittest.TestCase):
         self.mock_ingest_api = MagicMock(spec=IngestApi)
         self.data_collector = DataCollector(self.mock_ingest_api)
 
-    def test_collected_data_by_submission_returns_correctly(self):
+    def test_collected_project_and_biomaterials_data_by_submission_uuid_returns_correctly(self):
         #given
         project_uuid = '1234'
         with open('../resources/mock_submission.json') as file:
             mock_submission_json = json.load(file)
         with open('../resources/mock_project.json') as file:
             mock_project_json = json.load(file)
+        with open('../resources/mock_biomaterials.json') as file:
+            mock_biomaterials_json = json.load(file)
         self.mock_ingest_api.get_submission_by_uuid.return_value.json = mock_submission_json
-        self.mock_ingest_api.get_related_entities.return_value.json = mock_project_json
+
+        mock_data = [Mock(), Mock()]
+        mock_data[0].json = mock_project_json
+        mock_data[1].json = mock_biomaterials_json
+        self.mock_ingest_api.get_related_entities.side_effect = mock_data
         expected_json = [
-            mock_project_json
+            mock_project_json,
         ]
+        expected_json.extend(mock_biomaterials_json['_embedded']['biomaterials'])
 
         #when
         result_json = self.data_collector.collect_data_by_submission_uuid(project_uuid)
