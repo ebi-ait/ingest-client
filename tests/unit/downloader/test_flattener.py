6249cbc52c18ceae91f0649d2fa7297a8ef60970
Karoly Erdos <karoly@ebi.ac.uk>
2021-09-02 15:15:12 +0100

    Fix for empty ontology strings

--- tests/unit/downloader/test_flattener.py
+++ tests/unit/downloader/test_flattener.py
@@ -315,6 +315,50 @@ class FlattenerTest(TestCase):
         # then
         self.assertEqual(actual, self.flattened_metadata_entity)
 
+    def test_flatten__has_ontology_property_with_multiple_elements_but_with_empty_ontology_values(self):
+        # given
+        self.content.update(
+            {'diseases': [
+                {
+                    'ontology': 'UBERON:0000376',
+                    'ontology_label': 'dummylabel1',
+                    'text': 'dummytext1'
+                },
+                {
+                    'ontology': '',
+                    'ontology_label': '',
+                    'text': 'dummytext2'
+                }
+            ]})
+
+        self.metadata_entity = {
+            'content': self.content,
+            'uuid': self.uuid
+        }
+
+        entity_list = [self.metadata_entity]
+
+        # when
+        flattener = Flattener()
+        actual = flattener.flatten(entity_list)
+
+        self.flattened_metadata_entity['Project']['values'][0].update({
+            'project.diseases.ontology': 'UBERON:0000376',
+            'project.diseases.ontology_label': 'dummylabel1',
+            'project.diseases.text': 'dummytext1||dummytext2',
+
+        })
+        self.flattened_metadata_entity['Project']['headers'].extend(
+            [
+                'project.diseases.ontology',
+                'project.diseases.ontology_label',
+                'project.diseases.text'
+            ]
+        )
+
+        # then
+        self.assertEqual(self.flattened_metadata_entity, actual)
+
     def test_flatten__has_ontology_property_with_single_element_but_only_with_text_attr(self):
         # given
         self.content.update(
