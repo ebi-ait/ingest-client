f9f386dcddb0a5d34b336c464e3526e21f49284f
Jacob Windsor <me@jcbwndsr.com>
2021-10-27 17:14:20 +0200

    Use requirements in file for setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 base_dir = os.path.dirname(__file__)
-install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.txt'))]
+install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.in'))]
 
 setup(
     name='hca_ingest',
