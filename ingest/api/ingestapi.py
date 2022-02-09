94d54dfb56c4b8838c7678168263aaf2f1fbd866
Jacob Windsor <me@jcbwndsr.com>
2022-02-09 16:07:47 +0100

    dcp-618 allow no token when linking

--- ingest/api/ingestapi.py
+++ ingest/api/ingestapi.py
@@ -443,10 +443,8 @@ class IngestApi:
 
         self.logger.info('fromUri ' + from_uri + ' toUri:' + to_uri)
 
-        headers = {
-            'Content-type': 'text/uri-list',
-            'Authorization': self.get_headers()['Authorization']
-        }
+        headers = self.get_headers()
+        headers['Content-type'] = 'text/uri-list'
 
         if is_collection:
             r = self.session.post(from_uri.rsplit("{")[0],
