dd178af155826f94759ac58a25f954024138a089
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-09-13 13:23:35 +0100

    make requirements match hca-util

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 from setuptools import setup, find_packages
 
 base_dir = os.path.dirname(__file__)
-install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.txt'))]
+install_requires = [line.rstrip() for line in open(os.path.join(base_dir, 'requirements.in'))]
 
 with open("README.md", "r", encoding="utf-8") as md:
     long_description = md.read()
 
 setup(
     name='hca-ingest',
-    version='2.3.0',
+    version='2.4.0',
     description='A library to communicate with the Human Cell Atlas ingest API hosted by the EBI for creation and management of HCA project submissions.',
     long_description=long_description,
     long_description_content_type="text/markdown",
