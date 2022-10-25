dba48f9d0f6d0eac5b507c16f6919fc8f8e91f69
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-10-25 11:43:48 +0100

    dcp-845 more interpolation fixes

--- hca_ingest/api/ingestapi.py
+++ hca_ingest/api/ingestapi.py
@@ -185,11 +185,11 @@ class IngestApi:
         return self.get_all(user_project_url, 'projects')
 
     def get_entity_by_uuid(self, entity_type, uuid):
-        url = f'{self.url}/{entity_type}/search/findByUuid?uuid=' + uuid
+        url = f'{self.url}/{entity_type}/search/findByUuid?uuid={uuid}'
 
         # TODO make the endpoint consistent
         if entity_type == 'submissionEnvelopes':
-            url = f'{self.url}/{entity_type}/search/findByUuidUuid?uuid=' + uuid
+            url = f'{self.url}/{entity_type}/search/findByUuidUuid?uuid={uuid}'
 
         return self.get(url).json()
 
