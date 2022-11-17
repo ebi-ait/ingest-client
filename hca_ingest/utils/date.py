2fb30f46f697332329354809eeba178b43c01dc8
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-11-17 15:17:24 +0000

    dcp-873 use fromisoformat improvement

--- hca_ingest/utils/date.py
+++ hca_ingest/utils/date.py
@@ -1,20 +1,11 @@
 from datetime import datetime
 
-INGEST_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
-INGEST_DATE_FORMAT_SHORT = "%Y-%m-%dT%H:%M:%SZ"
 
-_expected_formats = [INGEST_DATE_FORMAT, INGEST_DATE_FORMAT_SHORT]
-
-
-def parse_date_string(date_str: str):
-    for date_format in _expected_formats:
-        try:
-            return datetime.strptime(date_str, date_format)
-        except ValueError:
-            pass
-    raise ValueError(f'unknown date format for [{date_str}]')
+def parse_date_string(date_str: str) -> datetime:
+    edited_date = date_str.removesuffix('Z')
+    return datetime.fromisoformat(edited_date)
 
 
-def date_to_json_string(date: datetime):
+def date_to_json_string(date: datetime) -> str:
     return date.isoformat().replace("+00:00", "Z")
 
