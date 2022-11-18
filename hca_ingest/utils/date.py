7b64eb0cd0c6e02d317d7dd610ac377361a304b7
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2022-11-18 17:32:28 +0000

    date parsing utils

--- hca_ingest/utils/date.py
+++ hca_ingest/utils/date.py
@@ -3,6 +3,9 @@ from datetime import datetime
 
 def parse_date_string(date_str: str) -> datetime:
     edited_date = date_str.removesuffix('Z')
+    date_time, dot, milliseconds = edited_date.partition('.')
+    if milliseconds:
+        edited_date = date_time + dot + milliseconds.ljust(6, '0')
     return datetime.fromisoformat(edited_date)
 
 
