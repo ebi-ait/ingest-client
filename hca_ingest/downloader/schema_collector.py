1b1909b6575bb7f6f09c5fc730f046654828d21b
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2023-01-19 13:48:54 +0000

    dcp-544 schema_collector cleanup

--- hca_ingest/downloader/schema_collector.py
+++ hca_ingest/downloader/schema_collector.py
@@ -10,13 +10,9 @@ class SchemaCollector:
     def __init__(self):
         self.schema_cache = {}
 
-    def get_schemas_from_entities(self, entity_list: Iterable[Entity]) -> dict:
+    def get_schemas_for_entities(self, entity_list: Iterable[Entity]) -> dict:
         schema_urls = self.get_schema_urls(entity_list)
-        duplicate_schemas = self.duplicate_schemas_by_concrete_type(schema_urls)
-        if duplicate_schemas:
-            raise ValueError(f'The concrete entity schema version should be consistent across entities. '
-                             f'Multiple versions of same concrete entity schema found: '
-                             f'{[schema.url for schema in duplicate_schemas]}')
+        self.check_for_duplicate_schemas(schema_urls)
         return { schema.url : self.__get_schema_from_cache(schema.url) for schema in schema_urls }
 
     def __get_schema_from_cache(self, url: str) -> dict:
@@ -31,14 +27,22 @@ class SchemaCollector:
         return set([entity.schema for entity in entity_list])
 
     @staticmethod
-    def duplicate_schemas_by_concrete_type(schema_urls: set[SchemaUrl]) -> set[SchemaUrl]:
-        concrete_types = [url.concrete_type for url in schema_urls]
+    def check_for_duplicate_schemas(schema_urls: set[SchemaUrl]):
+        duplicate_schemas = SchemaCollector.get_duplicate_schemas_by_concrete_type(schema_urls)
+        if duplicate_schemas:
+            raise ValueError(f'The concrete entity schema version should be consistent across entities. '
+                             f'Multiple versions of same concrete entity schema found: '
+                             f'{[schema.url for schema in duplicate_schemas]}')
+
+    @staticmethod
+    def get_duplicate_schemas_by_concrete_type(schema_urls: set[SchemaUrl]) -> set[SchemaUrl]:
+        concrete_types = [schema.concrete_type for schema in schema_urls]
         distinct_types = set(concrete_types)
         duplicate_types = concrete_types.copy()
         for type in distinct_types:
             duplicate_types.remove(type)
         duplicate_types = set(duplicate_types)
-        return set(filter(lambda url: url.concrete_type in duplicate_types, schema_urls))
+        return set(filter(lambda schema: schema.concrete_type in duplicate_types, schema_urls))
 
     @staticmethod
     def __get_schema(url: str)-> dict:
--- hca_ingest/downloader/workbook.py
+++ hca_ingest/downloader/workbook.py
@@ -17,6 +17,6 @@ class WorkbookDownloader:
     def get_workbook_from_submission(self, submission_uuid: str) -> Workbook:
         submission_entities = self.data_collector.collect_data_by_submission_uuid(submission_uuid)
         entities_with_content = list(filter(lambda entity: entity.content, submission_entities.values()))
-        schemas = self.schema_collector.get_schemas_from_entities(entities_with_content)
+        schemas = self.schema_collector.get_schemas_for_entities(entities_with_content)
         flattened_json = self.flattener.flatten(entities_with_content, schemas)
         return self.downloader.create_workbook(flattened_json)
