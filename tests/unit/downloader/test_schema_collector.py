d5ac2d9a1da5101e0babf7750bdcd68a4ab2f4a1
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2023-01-20 17:39:39 +0000

    dcp-544 duplicate concrete type tests

--- tests/unit/downloader/test_schema_collector.py
+++ tests/unit/downloader/test_schema_collector.py
@@ -10,14 +10,6 @@ from .conftest import get_json_file
 
 
 @pytest.fixture
-def multiple_schema_versions_of_same_concrete_entity() -> set[SchemaUrl]:
-    return {
-        SchemaUrl("https://schema.humancellatlas.org/type/biomaterial/14.2.0/donor_organism"),
-        SchemaUrl("https://schema.humancellatlas.org/type/biomaterial/14.3.0/donor_organism")
-    }
-
-
-@pytest.fixture
 def schema_cache(script_dir):
     return get_json_file(script_dir + '/schema_cache.json')
 
@@ -28,11 +20,6 @@ def schema_collector(schema_cache):
     schema_collector.schema_cache.update(schema_cache)
     return schema_collector
 
-
-def test_collection_raises_error(multiple_schema_versions_of_same_concrete_entity, schema_collector):
-    with pytest.raises(ValueError) as value_error:
-        schema_collector.get_schemas(multiple_schema_versions_of_same_concrete_entity)
-        assert_that(str(value_error.value)).is_equal_to("Multiple versions of same concrete entity schema")
 
 @pytest.fixture
 def url():
@@ -78,3 +65,52 @@ def test_collector_gets_schema_from_cache_and_internet(schema_collector, schema_
 
         schema_collector.get_schemas({schema_url})
         assert_that(mock.call_count).is_equal_to(1)
+
+
+@pytest.fixture
+def multiple_schema_versions_of_same_concrete_entity() -> set[SchemaUrl]:
+    return {
+        SchemaUrl("https://schema.humancellatlas.org/type/biomaterial/14.2.0/donor_organism"),
+        SchemaUrl("https://schema.humancellatlas.org/type/biomaterial/14.3.0/donor_organism")
+    }
+
+
+
+def test_collection_raises_error(multiple_schema_versions_of_same_concrete_entity, schema_collector):
+    with pytest.raises(ValueError) as value_error:
+        schema_collector.get_schemas(multiple_schema_versions_of_same_concrete_entity)
+        assert_that(str(value_error.value)).is_equal_to("Multiple versions of same concrete entity schema")
+
+
+@pytest.fixture
+def duplicate_concrete_type_schema_urls():
+    return [
+        SchemaUrl('https://schema/type/domainA/version1/concreteA'),
+        SchemaUrl('https://schema/type/domainB/version1/concreteB'),
+        SchemaUrl('https://schema/type/domainC/version1/concreteC'),
+        SchemaUrl('https://schema/type/domainD/version1/concreteD'),
+        SchemaUrl('https://schema/type/domainA/version1/concreteA'),
+        SchemaUrl('https://schema/type/domainB/version1/concreteB'),
+        SchemaUrl('https://schema/type/domainC/version1/concreteC'),
+        SchemaUrl('https://schema/type/domainD/version1/concreteD'),
+        SchemaUrl('https://schema/type/domainA/version2/concreteA'),
+        SchemaUrl('https://schema/type/domainD/version2/concreteD')
+    ]
+
+
+@pytest.fixture
+def expected_duplicates():
+    return {
+        SchemaUrl('https://schema/type/domainA/version1/concreteA'),
+        SchemaUrl('https://schema/type/domainD/version1/concreteD'),
+        SchemaUrl('https://schema/type/domainA/version2/concreteA'),
+        SchemaUrl('https://schema/type/domainD/version2/concreteD')
+    }
+
+
+def test_get_duplicate_schemas_by_concrete_type(duplicate_concrete_type_schema_urls, schema_collector, expected_duplicates):
+    assert_that(duplicate_concrete_type_schema_urls).is_length(10)
+    schema_urls = set(duplicate_concrete_type_schema_urls)
+    assert_that(schema_urls).is_length(6)
+    duplicate_uls = schema_collector.get_duplicate_schemas_by_concrete_type(schema_urls)
+    assert_that(duplicate_uls).is_equal_to(expected_duplicates)
