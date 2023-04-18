252141a5ce471049c0403c682a19ddc9cff407cc
Alegria Aclan <aaclan@ebi.ac.uk>
2023-04-18 20:32:08 +0100

    dcp-928 Remove unused method

--- hca_ingest/downloader/data_collector.py
+++ hca_ingest/downloader/data_collector.py
@@ -108,29 +108,6 @@ class DataCollector:
 
                 entity.set_input(input_biomaterials, input_files, process, protocols)
 
-    def __set_inputs(self, entity_dict):
-        for entity_id in entity_dict:
-            entity = entity_dict[entity_id]
-            if entity.schema.domain_type in ['biomaterial', 'file']:
-                derived_by_processes = [Entity(entity) for entity in
-                                        self.api.get_related_entities('derivedByProcesses', entity.entity_json,
-                                                                      'processes')]
-
-                if derived_by_processes and len(derived_by_processes) > 0:
-                    # Check if derivedByProcesses returns more than 1
-                    # It shouldn't happen because it's not possible to do it via spreadsheet
-                    if len(derived_by_processes) > 1:
-                        raise ValueError(f'The {entity.schema.concrete_type} with {entity.uuid} '
-                                         f'has more than one processes which derived it')
-                    process = derived_by_processes[0]
-                    protocols = [Entity(protocol) for protocol in
-                                 list(self.api.get_related_entities('protocols', process.entity_json, 'protocols'))]
-                    input_biomaterials = [Entity(biomaterial) for biomaterial in list(
-                        self.api.get_related_entities('inputBiomaterials', process.entity_json, 'biomaterials'))]
-                    input_files = [Entity(file) for file in
-                                   list(self.api.get_related_entities('inputFiles', process.entity_json, 'files'))]
-                    entity.set_input(input_biomaterials, input_files, process, protocols)
-
     def __get_entities_by_submission_and_type(self, submission, entity_type):
         self.api.page_size = 10
         yield from self.api.get_related_entities(entity_type, submission, entity_type)
