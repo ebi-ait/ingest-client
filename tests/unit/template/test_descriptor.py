2e080c67cf2c0415617032eee4af708912046017
Alexandros Orges Koci <koci.orges@hotmail.com>
2023-11-01 22:19:24 +0000

    mc-30 Added test case for SimplePropertyDescriptor with 'title' instead of 'user_friendly' property

--- tests/unit/template/test_descriptor.py
+++ tests/unit/template/test_descriptor.py
@@ -107,6 +107,30 @@ class TestDescriptor(unittest.TestCase):
         }
         self.assertEqual(descriptor.get_dictionary_representation_of_descriptor(), expected_dictionary_representation)
 
+    def test__simple_property_descriptor_with_title_instead_of_user_friendly_property__succeeds(self):
+        sample_simple_property_description = "The version number of the schema in major.minor.patch format."
+        sample_simple_property_type = "string"
+        sample_simple_property_pattern = "^[0-9]{1,}.[0-9]{1,}.[0-9]{1,}$"
+        sample_simple_property_example = "4.6.1"
+        sample_simple_property_user_friendly = "Project description"
+        sample_simple_metadata_schema_json = {
+
+            "description": sample_simple_property_description,
+            "type": sample_simple_property_type,
+            "pattern": sample_simple_property_pattern,
+            "example": sample_simple_property_example,
+            "title": sample_simple_property_user_friendly
+        }
+
+        descriptor = SimplePropertyDescriptor(sample_simple_metadata_schema_json)
+
+        expected_dictionary_representation = {
+            "description": sample_simple_property_description, "value_type": sample_simple_property_type,
+            "example": sample_simple_property_example, "user_friendly": sample_simple_property_user_friendly,
+            "multivalue": False, "external_reference": False, "required": False, "identifiable": False
+        }
+        self.assertEqual(descriptor.get_dictionary_representation_of_descriptor(), expected_dictionary_representation)
+
     def test__simple_array_property_descriptor__succeeds(self):
         sample_simple_property_description = "The version number of the schema in major.minor.patch format."
         sample_simple_property_type = "array"
