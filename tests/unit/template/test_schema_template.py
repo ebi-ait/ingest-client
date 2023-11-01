d894ff062ebd98b96fde83cc6561e10b3e93b426
Alexandros Orges Koci <koci.orges@hotmail.com>
2023-11-01 21:55:39 +0000

    Added test case for creation of template for JSON with 'content' property

--- tests/unit/template/test_schema_template.py
+++ tests/unit/template/test_schema_template.py
@@ -71,6 +71,67 @@ class TestSchemaTemplate(unittest.TestCase):
         self.assertEqual(schema_template.tabs, expected_schema_tabs)
 
     @patch("requests.get")
+    def test__creation_of_template_with_json_with_content_property__success(self, property_migrations_request_mock):
+        property_migrations_request_mock.return_value = Mock(ok=True)
+        property_migrations_request_mock.return_value.json.return_value = {'migrations': []}
+
+        sample_metadata_schema_json = {
+            "$schema": "https://json-schema.org/draft/2019-09/schema",
+            "$id": "https://d1wew2wvfg0okw.cloudfront.net/type/project/1.0.0/study",
+            "description": "A study entity defines the fields needed for representing a study.",
+            "required": [
+                "content"
+            ],
+            "type": "object",
+            "properties": {
+                "content": {
+                    "description": "A study entity defines the fields needed for representing a study.",
+                    "required": [
+                        "contact_first_name",
+                        "contact_surname"
+                    ],
+                    "type": "object",
+                    "user_friendly": "Content",
+                    "properties": {
+                        "contact_first_name": {
+                            "description": "First name of the main person to contact for queries about this study.",
+                            "user_friendly": "Contact first name",
+                            "type": "string",
+                            "minLength": 1,
+                            "example": "Anu; Enrique; Galabina"
+                        },
+                        "contact_surname": {
+                            "description": "Surname of the main person to contact for queries about this study.",
+                            "user_friendly": "Contact surname",
+                            "type": "string",
+                            "minLength": 1,
+                            "example": "Koci; Zucchi"
+                        }
+                    }
+                }
+            }
+        }
+
+        schema_template = SchemaTemplate(json_schema_docs=[sample_metadata_schema_json])
+
+        expected_schema_version = "1.0.0"
+        expected_schema_metadata_properties = {
+            "study": SchemaParser(sample_metadata_schema_json).schema_dictionary}
+        expected_schema_labels = {"study.content": ["study.content"], "content": ["study.content"],
+                                  "study.content.contact_first_name": ["study.content.contact_first_name"],
+                                  "contact first name": ["study.content.contact_first_name"],
+                                  "study.content.contact_surname": ["study.content.contact_surname"],
+                                  "contact surname": ["study.content.contact_first_name"]}
+        expected_schema_tabs = [{"study": {"display_name": "Study",
+                                           "columns": ["study.content.contact_first_name",
+                                                       "study.content.contact_surname"]}}]
+
+        self.assertEqual(schema_template.template_version, expected_schema_version)
+        self.assertEqual(schema_template.meta_data_properties, expected_schema_metadata_properties)
+        self.assertEqual(schema_template.labels, expected_schema_labels)
+        self.assertEqual(schema_template.tabs, expected_schema_tabs)
+
+    @patch("requests.get")
     def test__get_list_of_schema_spreadsheet_representations__success(self, property_migrations_request_mock):
         property_migrations_request_mock.return_value = Mock(ok=True)
         property_migrations_request_mock.return_value.json.return_value = {'migrations': []}
