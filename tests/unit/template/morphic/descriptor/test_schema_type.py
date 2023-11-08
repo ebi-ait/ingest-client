7a4abaa9f698bdc08e4c83c05eb7d62caad90940
Alexandros Orges Koci <koci.orges@hotmail.com>
2023-11-08 22:48:27 +0000

    mc-30: Added test cases for SchemaType descriptor

---
+++ tests/unit/template/morphic/descriptor/test_schema_type.py
@@ -0,0 +1,23 @@
+import unittest
+import pytest
+
+from hca_ingest.template.descriptor import SchemaTypeDescriptor
+
+
+@pytest.fixture
+def metadata_schema_url():
+    return "https://d1wew2wvfg0okw.cloudfront.net"
+
+
+class TestSchemaTypeDescriptor:
+    """ Testing class for the SchemaTypeDescriptor of the Descriptor class. """
+
+    def test__schema_type_descriptor__succeeds(self, metadata_schema_url):
+        sample_metadata_schema_url = metadata_schema_url + "/type/biomaterial/10.0.2/organoid"
+
+        descriptor = SchemaTypeDescriptor(sample_metadata_schema_url)
+
+        expected_dictionary_representation = {"high_level_entity": "type", "domain_entity": "biomaterial",
+                                              "module": "organoid", "version": "10.0.2",
+                                              "url": sample_metadata_schema_url}
+        assert descriptor.get_dictionary_representation_of_descriptor() == expected_dictionary_representation
